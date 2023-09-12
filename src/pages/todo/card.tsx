75179d5651048d75e8d825ed8325afc9bda14fb3
“John <johnswaroop28@gmail.com>
2023-09-12 23:23:55 +0530

    status

--- src/pages/todo/card.tsx
+++ src/pages/todo/card.tsx
@@ -1,35 +1,55 @@
 import React from "react";
 import styles from "./todo.module.scss";
 
+import { todosType } from "./index";
+
 let priorityObj = {
-    high:<button style={{color:'#ff9e21',borderColor:'#ff9e21'}}>High</button>,
-    medium:<button style={{color:'#41c95f',borderColor:'#41c95f'}}>Medium</button>,
-    low:<button style={{color:'#2ccdcd',borderColor:'#2ccdcd'}}>Low</button>
+  high: (
+    <button style={{ color: "#ff9e21", borderColor: "#ff9e21" }}>High</button>
+  ),
+  medium: (
+    <button style={{ color: "#41c95f", borderColor: "#41c95f" }}>Medium</button>
+  ),
+  low: (
+    <button style={{ color: "#2ccdcd", borderColor: "#2ccdcd" }}>Low</button>
+  ),
 };
 let arr = [
-    { title: "Todo", color: "#747474" },
-    { title: "Doing", color: "#ff9e21" },
-    { title: "In Review", color: "#2ccdcd" },
-    { title: "Done", color: "#41c95f" },
-  ];
+  { title: "Todo", color: "#747474" },
+  { title: "Doing", color: "#ff9e21" },
+  { title: "In Review", color: "#2ccdcd" },
+  { title: "Done", color: "#41c95f" },
+];
 
-function Card({status}:{status:string}) {
+function Card({ data }: { data: todosType }) {
   return (
     <div className={styles.card}>
       <div className={styles.pr_wrap}>
-         {priorityObj['medium']}
-         <svg xmlns="http://www.w3.org/2000/svg" height="1em" viewBox="0 0 448 512"><path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z"/></svg>
+        {priorityObj[data.priority as "high"]}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          height="1em"
+          viewBox="0 0 448 512"
+        >
+          <path d="M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z" />
+        </svg>
       </div>
       <div className={styles.card_Cont}>
-        <h4>john wick</h4>
-        <p>Lorem ipsum dolor sit.</p>
+        <h4>{data.title}</h4>
+        <p>{data.description}</p>
       </div>
       <select name="pets" id="pet-select">
-      {
-        arr.filter((ele,ind)=>{return ele.title !== status}).map((ele,ind)=>{
-           return <option value={ele.title} key={'dd'+ind}>{ele.title}</option>
-        })
-      }
+        {arr
+          .filter((ele, ind) => {
+            return ele.title !== data.status;
+          })
+          .map((ele, ind) => {
+            return (
+              <option value={ele.title} key={"dd" + ind}>
+                {ele.title}
+              </option>
+            );
+          })}
       </select>
     </div>
   );
--- src/pages/todo/index.tsx
+++ src/pages/todo/index.tsx
@@ -1,56 +1,207 @@
-import React, { useState } from "react";
+import React, { useState, createContext, useContext, useReducer } from "react";
 import styles from "./todo.module.scss";
 import Card from "./card";
+import { v4 } from "uuid";
+
+let TodoContext = createContext({});
+
+export interface todosType {
+  id: string;
+  title: string;
+  description: string;
+  status: "Todo" | "Doing" | "In Review" | "Done";
+  priority: string;
+}
 
 function Todo() {
-  let status = [
-    { title: "Todo", color: "#747474" },
-    { title: "Doing", color: "#ff9e21" },
-    { title: "In Review", color: "#2ccdcd" },
-    { title: "Done", color: "#41c95f" },
-  ];
+  const [globalTodo, setGlobalTodo] = useState<todosType[]>([]);
+
+  console.log(globalTodo);
+
   return (
     <>
-      <div className={styles.main_cont}>
-        {status.map((ele, ind) => {
-          return <Box key={"ted" + ind} title={ele.title} color={ele.color} />;
-        })}
-      </div>
+      <TodoContext.Provider value={{ globalTodo, setGlobalTodo } as any}>
+        <div className={styles.main_cont}>
+          {status.map((ele, ind) => {
+            return (
+              <Box key={"ted" + ind} status={ele.title} color={ele.color} />
+            );
+          })}
+        </div>
+      </TodoContext.Provider>
     </>
   );
 }
 
+let status = [
+  { title: "Todo", color: "#747474" },
+  { title: "Doing", color: "#ff9e21" },
+  { title: "In Review", color: "#2ccdcd" },
+  { title: "Done", color: "#41c95f" },
+];
+
 interface boxType {
-  title: string;
+  status: string;
   color: string;
 }
-function Box({ title, color }: boxType) {
-  let [showModal,setModal] = useState<boolean>(false);
+
+function Box({ status, color }: boxType) {
+  let [showModal, setModal] = useState<boolean>(false);
+  let context = useContext(TodoContext) as any;
+  let todos = context.globalTodo as todosType[];
+
   return (
     <div className={styles.box}>
       <div className={styles.header}>
         <div className={styles.brand}>
-          <h4>{title}</h4>
+          <h4>{status}</h4>
           <span style={{ background: color }}>
             <p>1</p>
           </span>
         </div>
-        <button onClick={() =>{setModal(true)}}>+</button>
+        <button
+          onClick={() => {
+            setModal(true);
+          }}
+        >
+          +
+        </button>
       </div>
       <div className={styles.cards}>
-        <Card status={title}/>
+        {todos
+          .filter((c) => c.status === status)
+          .map((ele, idx) => {
+            return <Card data={ele as todosType} key={"card" + idx} />;
+          })}
       </div>
-      {
-        showModal && <Modal currentStatus={title}/>
-      }
+      {showModal && (
+        <Modal
+          currentStatus={status}
+          close={() => {
+            setModal(false);
+          }}
+        />
+      )}
     </div>
   );
 }
 
-function Modal({currentStatus}:{currentStatus:string}){
-  return <div className={styles.modal}>
-    <div className={styles.modal_box}></div>
-  </div>
+interface taskType {
+  id: string;
+  title: string;
+  description: string;
+  priority: string;
+  status: string;
 }
+
+function Modal({
+  currentStatus,
+  close,
+}: {
+  currentStatus: string;
+  close: () => void;
+}) {
+  let context = useContext(TodoContext) as any;
+
+  const [formState, setformState] = useState<taskType>({
+    id: v4() as string,
+    title: "",
+    description: "",
+    priority: "",
+    status: currentStatus,
+  });
+
+  const submitForm = (e: any) => {
+    e.preventDefault();
+    context.setGlobalTodo((st: any) => {
+      st.push(formState);
+      return [...st];
+    });
+  };
+
+  return (
+    <div className={styles.modal}>
+      <form
+        onSubmit={(e) => {
+          submitForm(e);
+        }}
+        className={styles.modal_box}
+      >
+        <h1>Enter Task Details</h1>
+        <label htmlFor="">
+          <p>Title</p>
+          <input
+            required
+            value={formState.title}
+            onChange={(e) => {
+              setformState((f) => {
+                f.title = e.target.value;
+                return { ...f };
+              });
+            }}
+            type="text"
+            name=""
+            id=""
+          />
+        </label>
+        <label htmlFor="">
+          <p>Description</p>
+          <textarea
+            required
+            value={formState.description}
+            onChange={(e) => {
+              setformState((f) => {
+                f.description = e.target.value;
+                return { ...f };
+              });
+            }}
+            rows={10}
+            name=""
+            id=""
+          />
+        </label>
+        <select
+          name=""
+          id=""
+          onChange={(e) => {
+            setformState((f) => {
+              f.priority = e.target.value;
+              return { ...f };
+            });
+          }}
+        >
+          {Object.keys(priorityObj).map((ele) => {
+            return (
+              <option key={ele + "opt"} value={ele}>
+                {ele}
+              </option>
+            );
+          })}
+        </select>
+        <button type="submit">Submit</button>
+        <button
+          type="button"
+          onClick={() => {
+            close();
+          }}
+        >
+          Close
+        </button>
+      </form>
+    </div>
+  );
+}
+
+let priorityObj = {
+  high: (
+    <button style={{ color: "#ff9e21", borderColor: "#ff9e21" }}>High</button>
+  ),
+  medium: (
+    <button style={{ color: "#41c95f", borderColor: "#41c95f" }}>Medium</button>
+  ),
+  low: (
+    <button style={{ color: "#2ccdcd", borderColor: "#2ccdcd" }}>Low</button>
+  ),
+};
 
 export default Todo;
